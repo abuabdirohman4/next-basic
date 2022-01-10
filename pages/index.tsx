09c505f76c32c374fe567e2d0e72a8d0e6028834
abuabdirohman4 <abuabdirohman4@gmail.com>
2022-01-10 23:49:32 +0700

    update component link on index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,31 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
+import Link from 'next/link';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
-    <h1>Welcome Abu Abdirohman</h1>
+    <>
+      <h1>Welcome Abu Abdirohman</h1>
+
+      <header>
+        <ul>
+          <li>
+            <Link href="/">
+              <a>Home</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/blog">
+              Blog
+            </Link>
+          </li>
+          <li>
+            <a href="?users">Users</a>
+          </li>
+        </ul>
+      </header>
+    </>
   );
 }
