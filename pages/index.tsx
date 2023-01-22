cdfc1159ea69647865e78643b51236a28fa75ffb
abuabdirohman4 <abuabdirohman4@gmail.com>
2023-01-23 06:31:11 +0700

    #13: Optimalisasi Image

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Layout from "../components/Layout";
 import styles from "../styles/Home.module.css";
 
@@ -5,7 +6,49 @@ export default function Home() {
   return (
     <>
       <Layout pageTitle="Home Page">
+        <Image src="/profile.jpg" width={200} height={200} alt="profile"/>
+        {/* <img src="/profile.jpg" width={200} height={200} alt="profile" /> */}
         <h1 className={styles["title-homepage"]}>Welcome Abu Abdirohman</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos repudiandae deserunt adipisci eveniet voluptate sapiente nulla consequatur totam eaque illum assumenda excepturi corporis, laborum omnis libero qui. Repellendus, fuga neque!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos repudiandae deserunt adipisci eveniet voluptate sapiente nulla consequatur totam eaque illum assumenda excepturi corporis, laborum omnis libero qui. Repellendus, fuga neque!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos repudiandae deserunt adipisci eveniet voluptate sapiente nulla consequatur totam eaque illum assumenda excepturi corporis, laborum omnis libero qui. Repellendus, fuga neque!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos repudiandae deserunt adipisci eveniet voluptate sapiente nulla consequatur totam eaque illum assumenda excepturi corporis, laborum omnis libero qui. Repellendus, fuga neque!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos repudiandae deserunt adipisci eveniet voluptate sapiente nulla consequatur totam eaque illum assumenda excepturi corporis, laborum omnis libero qui. Repellendus, fuga neque!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos repudiandae deserunt adipisci eveniet voluptate sapiente nulla consequatur totam eaque illum assumenda excepturi corporis, laborum omnis libero qui. Repellendus, fuga neque!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos repudiandae deserunt adipisci eveniet voluptate sapiente nulla consequatur totam eaque illum assumenda excepturi corporis, laborum omnis libero qui. Repellendus, fuga neque!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos repudiandae deserunt adipisci eveniet voluptate sapiente nulla consequatur totam eaque illum assumenda excepturi corporis, laborum omnis libero qui. Repellendus, fuga neque!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos repudiandae deserunt adipisci eveniet voluptate sapiente nulla consequatur totam eaque illum assumenda excepturi corporis, laborum omnis libero qui. Repellendus, fuga neque!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos repudiandae deserunt adipisci eveniet voluptate sapiente nulla consequatur totam eaque illum assumenda excepturi corporis, laborum omnis libero qui. Repellendus, fuga neque!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos repudiandae deserunt adipisci eveniet voluptate sapiente nulla consequatur totam eaque illum assumenda excepturi corporis, laborum omnis libero qui. Repellendus, fuga neque!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos repudiandae deserunt adipisci eveniet voluptate sapiente nulla consequatur totam eaque illum assumenda excepturi corporis, laborum omnis libero qui. Repellendus, fuga neque!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos repudiandae deserunt adipisci eveniet voluptate sapiente nulla consequatur totam eaque illum assumenda excepturi corporis, laborum omnis libero qui. Repellendus, fuga neque!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos repudiandae deserunt adipisci eveniet voluptate sapiente nulla consequatur totam eaque illum assumenda excepturi corporis, laborum omnis libero qui. Repellendus, fuga neque!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos repudiandae deserunt adipisci eveniet voluptate sapiente nulla consequatur totam eaque illum assumenda excepturi corporis, laborum omnis libero qui. Repellendus, fuga neque!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos repudiandae deserunt adipisci eveniet voluptate sapiente nulla consequatur totam eaque illum assumenda excepturi corporis, laborum omnis libero qui. Repellendus, fuga neque!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos repudiandae deserunt adipisci eveniet voluptate sapiente nulla consequatur totam eaque illum assumenda excepturi corporis, laborum omnis libero qui. Repellendus, fuga neque!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos repudiandae deserunt adipisci eveniet voluptate sapiente nulla consequatur totam eaque illum assumenda excepturi corporis, laborum omnis libero qui. Repellendus, fuga neque!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos repudiandae deserunt adipisci eveniet voluptate sapiente nulla consequatur totam eaque illum assumenda excepturi corporis, laborum omnis libero qui. Repellendus, fuga neque!</p>
+        <Image src="/post1.jpg" width={200} height={200} alt="profile"/>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos repudiandae deserunt adipisci eveniet voluptate sapiente nulla consequatur totam eaque illum assumenda excepturi corporis, laborum omnis libero qui. Repellendus, fuga neque!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos repudiandae deserunt adipisci eveniet voluptate sapiente nulla consequatur totam eaque illum assumenda excepturi corporis, laborum omnis libero qui. Repellendus, fuga neque!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos repudiandae deserunt adipisci eveniet voluptate sapiente nulla consequatur totam eaque illum assumenda excepturi corporis, laborum omnis libero qui. Repellendus, fuga neque!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos repudiandae deserunt adipisci eveniet voluptate sapiente nulla consequatur totam eaque illum assumenda excepturi corporis, laborum omnis libero qui. Repellendus, fuga neque!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos repudiandae deserunt adipisci eveniet voluptate sapiente nulla consequatur totam eaque illum assumenda excepturi corporis, laborum omnis libero qui. Repellendus, fuga neque!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos repudiandae deserunt adipisci eveniet voluptate sapiente nulla consequatur totam eaque illum assumenda excepturi corporis, laborum omnis libero qui. Repellendus, fuga neque!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos repudiandae deserunt adipisci eveniet voluptate sapiente nulla consequatur totam eaque illum assumenda excepturi corporis, laborum omnis libero qui. Repellendus, fuga neque!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos repudiandae deserunt adipisci eveniet voluptate sapiente nulla consequatur totam eaque illum assumenda excepturi corporis, laborum omnis libero qui. Repellendus, fuga neque!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos repudiandae deserunt adipisci eveniet voluptate sapiente nulla consequatur totam eaque illum assumenda excepturi corporis, laborum omnis libero qui. Repellendus, fuga neque!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos repudiandae deserunt adipisci eveniet voluptate sapiente nulla consequatur totam eaque illum assumenda excepturi corporis, laborum omnis libero qui. Repellendus, fuga neque!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos repudiandae deserunt adipisci eveniet voluptate sapiente nulla consequatur totam eaque illum assumenda excepturi corporis, laborum omnis libero qui. Repellendus, fuga neque!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos repudiandae deserunt adipisci eveniet voluptate sapiente nulla consequatur totam eaque illum assumenda excepturi corporis, laborum omnis libero qui. Repellendus, fuga neque!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos repudiandae deserunt adipisci eveniet voluptate sapiente nulla consequatur totam eaque illum assumenda excepturi corporis, laborum omnis libero qui. Repellendus, fuga neque!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos repudiandae deserunt adipisci eveniet voluptate sapiente nulla consequatur totam eaque illum assumenda excepturi corporis, laborum omnis libero qui. Repellendus, fuga neque!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos repudiandae deserunt adipisci eveniet voluptate sapiente nulla consequatur totam eaque illum assumenda excepturi corporis, laborum omnis libero qui. Repellendus, fuga neque!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos repudiandae deserunt adipisci eveniet voluptate sapiente nulla consequatur totam eaque illum assumenda excepturi corporis, laborum omnis libero qui. Repellendus, fuga neque!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos repudiandae deserunt adipisci eveniet voluptate sapiente nulla consequatur totam eaque illum assumenda excepturi corporis, laborum omnis libero qui. Repellendus, fuga neque!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos repudiandae deserunt adipisci eveniet voluptate sapiente nulla consequatur totam eaque illum assumenda excepturi corporis, laborum omnis libero qui. Repellendus, fuga neque!</p>
+        <Image src="/post2.jpeg" width={200} height={200} alt="profile"/>
       </Layout>
     </>
   );
