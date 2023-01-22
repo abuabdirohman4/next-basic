f636c533983fde37109b35093f8f9671cff239a1
abuabdirohman4 <abuabdirohman4@gmail.com>
2023-01-23 06:53:39 +0700

    #15: Data Fetching getStaticProps

--- pages/users/index.tsx
+++ pages/users/index.tsx
@@ -1,9 +1,30 @@
-import Layout from "../../components/Layout";
+import Layout from '../../components/Layout';
 
-export default function index() {
+interface UsersProps {
+  dataUsers: Array<any>;
+}
+
+export default function index({ dataUsers }: UsersProps) {
+  console.log(dataUsers);
   return (
     <Layout pageTitle="Users Page">
       <p>Users Page</p>
+      {dataUsers.map((user, index) => (
+        <div key={index}>
+          <p>{user.name}</p>
+          <p>{user.email}</p>
+        </div>
+      ))}
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const dataUsers = await res.json();
+  return {
+    props: {
+      dataUsers,
+    },
+  };
 }
