46573be86872a8fd91f7bfc79e1522fe8c978f3f
abuabdirohman4 <abuabdirohman4@gmail.com>
2023-01-22 22:27:07 +0700

    #7: Membuat Component Lain

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,10 @@
-import Link from 'next/link';
+import Header from "../components/Header";
 
 export default function Home() {
   return (
     <>
       <h1>Welcome Abu Abdirohman</h1>
-
-      <header>
-        <ul>
-          <li>
-            <Link href="/">
-              <a>Home</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/blog">
-              Blog
-            </Link>
-          </li>
-          <li>
-            <a href="?users">Users</a>
-          </li>
-        </ul>
-      </header>
+      <Header />
     </>
   );
 }
