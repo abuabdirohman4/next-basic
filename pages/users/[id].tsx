aa5b51dae995bdb594d0315600ec35d65c54f6c6
abuabdirohman4 <abuabdirohman4@gmail.com>
2023-01-23 15:01:13 +0700

    #17: fix error on $16, fallback to false, correct like tutorial

--- pages/users/[id].tsx
+++ pages/users/[id].tsx
@@ -2,32 +2,32 @@
 import Layout from '../../components/Layout';
 
 interface User {
-  // id: number
+  id: number
   name: string
   email: string
   phone: string
   website: string
 }
-// interface UserDetailProps{
-//   // user:object
-//   user:User
-// }
-// export default function UserDetail(props:UserDetailProps) {
-export default function UserDetail(props:User) {
+interface UserDetailProps{
+  // user:object
+  user:User
+}
+export default function UserDetail(props:UserDetailProps) {
+// export default function UserDetail(props:User) {
   // const router = useRouter();
   // const { id } = router.query;
-  // const { user } = props;
-  const {
-    name, email, phone, website,
-  } = props;
+  const { user } = props;
+  // const {
+  //   name, email, phone, website,
+  // } = props;
 
   return (
     <Layout pageTitle="Users Detail">
       <h1>User Detail Page</h1>
-      <p>{name}</p>
-      <p>{email}</p>
-      <p>{phone}</p>
-      <p>{website}</p>
+      <p>{user.name}</p>
+      <p>{user.email}</p>
+      <p>{user.phone}</p>
+      <p>{user.website}</p>
     </Layout>
   );
 }
@@ -39,14 +39,15 @@ export async function getStaticPaths() {
   // interface Id {
   //   id: number
   // }
-  const paths = dataUsers.map((user:{id: number}) => ({
+  const paths = dataUsers.map((user:User) => ({
+  // const paths = dataUsers.map((user: {id: number}) => ({
     params: {
       id: `${user.id}`,
     },
   }));
   return {
     paths,
-    fallback: true,
+    fallback: false,
   };
 }
 
@@ -59,12 +60,14 @@ export async function getStaticProps(context: GetStaticProps) {
   const { id } = context.params;
   // console.log('id', id);
   const res = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
-  const {
-    name, email, phone, website,
-  } = await res.json();
+  // const {
+  //   name, email, phone, website,
+  // } = await res.json();
+  const user = await res.json();
   return {
     props: {
-      name, email, phone, website,
+      // name, email, phone, website,
+      user,
     },
   };
 }
